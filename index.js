63cef2be527a78f2f2aa9dd059a1bfa7b4a08187
TaranChase <chase.taran@gmail.com>
2025-09-14 01:06:22 +0300

    Testing function expressions and arrow functions.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// using function expression to divide a number by 100.
 divide = function(num) {
     return num/100;
 }
 console.log(divide(2000));
 
+// using arrow function to multiply a number by itself.
 let square = (a) => a*a;
 console.log(square(5));
 
+// using arrow function to add output the sum of two given numbers.
 let add = (a, b) => a+b;
 console.log(add(2, 2));
