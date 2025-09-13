510217037bbee4cc2616220dcbe3ef5447d7bba6
TaranChase <chase.taran@gmail.com>
2025-09-14 01:00:51 +0300

    Testing function expressions and arrow functions.

---
+++ index.js
@@ -0,0 +1,10 @@
+divide = function(num) {
+    return num/100;
+}
+console.log(divide(2000));
+
+let square = (a) => a*a;
+console.log(square(5));
+
+let add = (a, b) => a+b;
+console.log(add(2, 2));
